539265358b3525a50aa537d08cb61020f5c6fdca
Farhan Fadoo <farhanfadoo@farhans-imac.lan>
2020-12-16 20:22:07 -0800

    added a box and button to index.html and styled the box, added a style.css file as well

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
 
@@ -15,4 +18,4 @@ app=Flask(__name__)
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
