687d6774251b67bc9aedacaca660fa4a39b257d2
Farhan Fadoo <farhanfadoo@farhans-imac.lan>
2021-01-06 20:18:38 -0800

    intigrated mongodb into the program, now the data entered by the user goes directly into the database, the connection string is now stored in a file to prevent it from being stored in github, we included the file in .gitignore to prevent it from going to github

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request
+import pymongo
 app=Flask(__name__)
+file=open("connectionstring.txt",'r')
+connectionstring=file.read()
+print(connectionstring)
+print("hello")
+# client=pymongo.MongoClient(connectionstring)
+# database=client["notemanager"]
+# mycollection=database["notes"]
 
 
 
-@app.route("/",methods=["GET","POST"])
-def index():
-    if request.method=="GET":
-        return render_template("index.html")
-    else:
-        name=request.form["namebox"]
-        note=request.form["notebox"]
-        print(name,note)
-        return("information is submitted")
+# @app.route("/",methods=["GET","POST"])
+# def index():
+#     if request.method=="GET":
+#         return render_template("index.html")
+#     else:
+#         name=request.form["namebox"]
+#         note=request.form["notebox"]
+#         record={"name":name,"note":note}
+#         mycollection.insert_one(record)
+#         print(name,note)
+#         return("information is submitted")
 
 
 
@@ -24,5 +34,5 @@ def index():
 
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+# if __name__=="__main__":
+#     app.run(debug=True)
